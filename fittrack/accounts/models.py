9b856045331f809dc163f758464e8ea274e3b4a0
Colin Keogh <colin-182@hotmail.com>
2026-06-09 21:46:24 +0100

    models.py updated with models for customeruser

--- fittrack/accounts/models.py
+++ fittrack/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('member', 'Member'),
+        ('trainer', 'Trainer'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='member')
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True)
+    phone = models.CharField(max_length=20, blank=True)
